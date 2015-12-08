6aa5cfdc5c14a410cb5b52df0fa5b1b3ba6182b3
MURAKAMI Masahiko <m-murakami@esm.co.jp>
2015-12-08 22:14:19 +0900

    initial

---
+++ README.md
@@ -0,0 +1,59 @@
+# gasify
+
+[Browserify](http://browserify.org/) plugin for Google Apps Script.
+
+## About
+
+In Google Apps Script, it must be top level function declaration that entry point called from [google.script.run](https://developers.google.com/apps-script/guides/html/reference/run).
+When `gasify` detect a function assignment expression to `global` object. it generate a top level function declaration statement.
+
+## example
+
+main.js:
+```js
+var echo = require('./echo');
+global.echo = echo;
+```
+
+echo.js:
+```
+module.exports = function(message) {
+  return message;
+}
+```
+
+build:
+```
+$ browserify main.js -p gasify -o Code.gs
+```
+
+Code.gs
+```js
+var global = this;function echo() {
+}(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+module.exports = function(message) {
+  return message;
+};
+
+},{}],2:[function(require,module,exports){
+(function (global){
+var echo = require('./echo');
+global.echo = echo;
+
+}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
+},{"./echo":1}]},{},[2]);
+```
+
+## Installation
+
+```sh
+$ npm install gasify
+```
+
+## Usage
+
+### CLI
+
+```sh
+$ browserify main.js -p gasify -o Code.gs
+```
---
+++ index.js
@@ -0,0 +1,77 @@
+var through = require('through2');
+var stubs = require('./lib/stubs');
+
+module.exports = function (b, opts) {
+  var cache = {};
+  var stubsCache = {};
+
+  b.on('reset', collect);
+  collect();
+
+  function collect() {
+    cache = {};
+    b.pipeline.get('deps').push(through.obj(function (row, enc, next) {
+      var file = row.expose ? b._expose[row.id] : row.file;
+      cache[file] = {
+        source: row.source
+      };
+      this.push(row);
+      next();
+    }));
+  }
+
+  b.on('transform', function (tr, mfile) {
+    tr.on('file', function (dep) {
+      invalidate(mfile);
+    });
+  });
+
+  b.on('update', function (files) {
+    files.map(function (file) {
+      invalidate(file);
+    });
+  });
+
+  function invalidate(file) {
+    delete stubsCahce[file];
+  }
+
+  function generateEntryPoint() {
+    var entrypoints = '';
+    for (var file in cache) {
+      if ({}.hasOwnProperty.call(cache, file)) {
+        var stub = stubsCache[file];
+        if (!stub) {
+          stub = stubs.generateStubs(cache[file].source);
+          stubsCache[file] = stub;
+        }
+        entrypoints += stub;
+      }
+    }
+    return 'var global = this;' + entrypoints;
+  }
+
+  var createStream = function () {
+    var firstChunk = true;
+    var stream = through.obj(function (buf, enc, next) {
+      if (firstChunk) {
+        this.push(new Buffer(generateEntryPoint()));
+        firstChunk = false;
+      }
+      this.push(buf);
+      next();
+    });
+    stream.label = 'gasentrypoint';
+    return stream;
+  };
+
+  function setupPipeline() {
+    b.pipeline.get('wrap').push(createStream());
+  }
+
+  setupPipeline();
+
+  b.on('reset', function () {
+    setupPipeline();
+  });
+};
